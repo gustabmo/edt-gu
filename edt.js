40b290dd2a9f086cbd1ae6ef97a57fd06e36b6ea
Gustavo <gustavo@economatica.com.br>
2022-08-31 22:23:18 +0000

    edt.js avancando

--- edt.js
+++ edt.js
@@ -1,22 +1,32 @@
 function fillCalendar() {
   let sheet = SpreadsheetApp.getActiveSheet();
   let edts = sheet.getDataRange().getValues();
+  let exceptDates = [];
+  let hours = 0.0;
+  let ids = [];
 
-  line = 0;
+  let sheetLine = 0;
   edts.forEach ( function(edt) {
-    line++;
-    idsIn = edt[12].split(" \,\;");
-    if (edt[9] instanceof Date) {
-      exceptDates = [edt[9]];
-    } else {
-      exceptDates = edt[9].split(" \,\;");
-      exceptDates.foreach ( function(element,indexED,ED) {
-        ED[indexED] = Date(ED[indexED]);
-      })
-    }
+    sheetLine++;
     if ((edt[2] instanceof Date) && (edt[3] instanceof Number)) {
-      //
-      //
+      addThisEdt ( edt, hours, ids );
     }
   })
+}
+
+function addThisEdt ( edt, hours, ids ) {
+  let idsIn = edt[12].split(" \,\;");
+
+  if (edt[9] instanceof Date) {
+    let exceptDates = [edt[9]];
+  } else {
+    let exceptDates = edt[9].split(" \,\;");
+    exceptDates.foreach ( function(element,indexED,ExcD) {
+      ExcD[indexED] = Date(ExcD[indexED]);
+    })
+  }
+
+  for ( countSemanas=0; countSemanas<edt[3]; countSemanas++ ) {
+    //
+  }
 }
